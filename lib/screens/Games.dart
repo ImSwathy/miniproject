63a17448156e8dfe832c59c3a9d18a1717238fad
ImSwathy <swathysouparnika1211.com>
2023-04-25 20:23:49 +0530

    New code

---
+++ lib/screens/Games.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class GamesPage extends StatefulWidget {
+  @override
+  _GamesPageState createState() => _GamesPageState();
+}
+
+class _GamesPageState extends State<GamesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Games Page"),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+//import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_proj/screens/my_drawer_header.dart';
+import 'package:flutter_proj/screens/settings.dart';
 import 'package:flutter_proj/screens/signin_screen.dart';
-import 'package:flutter_proj/screens/signup_screen.dart';
+//import 'package:flutter_proj/screens/signup_screen.dart';
 
 import '../utils/color_utils.dart';
+import 'Leaderboard.dart';
+import 'dashboard.dart';
+import 'Games.dart';
+import 'notes.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,32 +19,120 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var currentPage = DrawerSections.dashboard;
+
   @override
   Widget build(BuildContext context) {
+    var container;
+    if (currentPage == DrawerSections.dashboard) {
+      container = DashboardPage();
+    } else if (currentPage == DrawerSections.Leaderboard) {
+      container = LeaderboardPage();
+    } else if (currentPage == DrawerSections.Games) {
+      container = GamesPage();
+    } else if (currentPage == DrawerSections.notes) {
+      container = NotesPage();
+    } else if (currentPage == DrawerSections.settings) {
+      container = SettingsPage();
+    }
     return Scaffold(
-        body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-              hexStringToColor('20539E'),
-              hexStringToColor('20797C'),
-              hexStringToColor('209E5A')
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(
-                    20, MediaQuery.of(context).size.height * .2, 20, 0),
-                child: ElevatedButton(
-                  child: const Text("Logout"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignInScreen()));
-                  },
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 28, 127, 232),
+        title: Text("Physioplay"),
+      ),
+      body: container,
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                MyHeaderDrawer(),
+                MyDrawerList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        // shows the list of menu drawer
+        children: [
+          menuItem(1, "Dashboard", Icons.dashboard_outlined,
+              currentPage == DrawerSections.dashboard ? true : false),
+          menuItem(2, "Leaderboard", Icons.people_alt_outlined,
+              currentPage == DrawerSections.Leaderboard ? true : false),
+          menuItem(3, "Games", Icons.event,
+              currentPage == DrawerSections.Games ? true : false),
+          menuItem(4, "Notes", Icons.notes,
+              currentPage == DrawerSections.notes ? true : false),
+          Divider(),
+          menuItem(5, "Settings", Icons.settings_outlined,
+              currentPage == DrawerSections.settings ? true : false),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage = DrawerSections.dashboard;
+            } else if (id == 2) {
+              currentPage = DrawerSections.Leaderboard;
+            } else if (id == 3) {
+              currentPage = DrawerSections.Games;
+            } else if (id == 4) {
+              currentPage = DrawerSections.notes;
+            } else if (id == 5) {
+              currentPage = DrawerSections.settings;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
                 ),
               ),
-            )));
+              Expanded(
+                flex: 3,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+enum DrawerSections {
+  dashboard,
+  Leaderboard,
+  Games,
+  notes,
+  settings,
 }
